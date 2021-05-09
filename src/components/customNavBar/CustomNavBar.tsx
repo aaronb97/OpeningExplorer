26d51aacc2e83cd6ed71344a7d3ac3a8bab20e62
aaronb97 <aaron9becker7@gmail.com>
2021-05-08 22:17:54 -0400

    fix brand button routing

--- src/components/customNavBar/CustomNavBar.tsx
+++ src/components/customNavBar/CustomNavBar.tsx
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom';
 
 const CustomNavBar = () => (
   <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-    <Navbar.Brand href="#home">Opening Explorer</Navbar.Brand>
+    <Navbar.Brand as={Link} to="/">
+      Opening Explorer
+    </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
